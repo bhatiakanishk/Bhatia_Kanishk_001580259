028619fa36816f7b421f0b234cbda530ded6b78f
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-09-23 17:08:34 -0400

    Update Assignment1.java

---
+++ Assignment 1/src/model/Assignment1.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author kanis
+ */
+public class Assignment1 {
+    
+    private String name;
+    private String location;
+    private String date;
+    private int telephone;
+    private int fax;
+    private String mail;
+    private int ssn;
+    private int medrec;
+    private int hp; /*Health Plan*/
+    private int ba; /*Bank account*/
+    private String ln; /*Licence Number*/
+    private String vi; /*Vehichle id*/
+    private String di; /*Device id*/
+    private String li; /*LinkedIn*/
+    private String uc; /*Unique Code*/
+    /*private String photo;*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getFax() {
+        return fax;
+    }
+
+    public void setFax(int fax) {
+        this.fax = fax;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getMedrec() {
+        return medrec;
+    }
+
+    public void setMedrec(int medrec) {
+        this.medrec = medrec;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getBa() {
+        return ba;
+    }
+
+    public void setBa(int ba) {
+        this.ba = ba;
+    }
+
+    public String getLn() {
+        return ln;
+    }
+
+    public void setLn(String ln) {
+        this.ln = ln;
+    }
+
+    public String getVi() {
+        return vi;
+    }
+
+    public void setVi(String vi) {
+        this.vi = vi;
+    }
+
+    public String getDi() {
+        return di;
+    }
+
+    public void setDi(String di) {
+        this.di = di;
+    }
+
+    public String getLi() {
+        return li;
+    }
+
+    public void setLi(String li) {
+        this.li = li;
+    }
+
+    public String getUc() {
+        return uc;
+    }
+
+    public void setUc(String uc) {
+        this.uc = uc;
+    }
+    
+    
+
+}
