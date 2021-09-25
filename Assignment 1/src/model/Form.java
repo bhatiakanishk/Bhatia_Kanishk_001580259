5d4415ec8c60437597346afe39cd78d3accb20d5
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-09-25 13:52:10 -0400

    Create Form.java

---
+++ Assignment 1/src/model/Form.java
@@ -0,0 +1,199 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author kanis
+ */
+public class Form {
+    
+    private String name;
+    private String state;
+    private int dateMonth; private int dateDay; private int dateYear;
+    private int fax;
+    private String email;
+    private int ssn;
+    private int medRec;
+    private int health;
+    private int bankAcc;
+    private int licence;
+    private String vehID;
+    private String licencePlate;
+    private String devID;
+    private String linkedIn;
+    private int ip1; private int ip2; private int ip3; private int ip4;
+    private String uid;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getDateMonth() {
+        return dateMonth;
+    }
+
+    public void setDateMonth(int dateMonth) {
+        this.dateMonth = dateMonth;
+    }
+
+    public int getDateDay() {
+        return dateDay;
+    }
+
+    public void setDateDay(int dateDay) {
+        this.dateDay = dateDay;
+    }
+
+    public int getDateYear() {
+        return dateYear;
+    }
+
+    public void setDateYear(int dateYear) {
+        this.dateYear = dateYear;
+    }
+
+    public int getFax() {
+        return fax;
+    }
+
+    public void setFax(int fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getMedRec() {
+        return medRec;
+    }
+
+    public void setMedRec(int medRec) {
+        this.medRec = medRec;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getBankAcc() {
+        return bankAcc;
+    }
+
+    public void setBankAcc(int bankAcc) {
+        this.bankAcc = bankAcc;
+    }
+
+    public int getLicence() {
+        return licence;
+    }
+
+    public void setLicence(int licence) {
+        this.licence = licence;
+    }
+
+    public String getVehID() {
+        return vehID;
+    }
+
+    public void setVehID(String vehID) {
+        this.vehID = vehID;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getDevID() {
+        return devID;
+    }
+
+    public void setDevID(String devID) {
+        this.devID = devID;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public int getIp1() {
+        return ip1;
+    }
+
+    public void setIp1(int ip1) {
+        this.ip1 = ip1;
+    }
+
+    public int getIp2() {
+        return ip2;
+    }
+
+    public void setIp2(int ip2) {
+        this.ip2 = ip2;
+    }
+
+    public int getIp3() {
+        return ip3;
+    }
+
+    public void setIp3(int ip3) {
+        this.ip3 = ip3;
+    }
+
+    public int getIp4() {
+        return ip4;
+    }
+
+    public void setIp4(int ip4) {
+        this.ip4 = ip4;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+    
+}
