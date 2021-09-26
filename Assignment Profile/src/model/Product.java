86241b051a17c52fa71fa2d272f1a0d9f5e9da9f
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-09-26 18:47:09 -0400

    Update Product.java

--- Assignment Profile/src/model/Product.java
+++ Assignment Profile/src/model/Product.java
@@ -36,6 +36,192 @@ public class Product {
     private int ip4; /*IP Address*/
     private int uid; /*Unique ID*/
 
+    public Scanner getSc() {
+        return sc;
+    }
+
+    public void setSc(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getDateDay() {
+        return dateDay;
+    }
+
+    public void setDateDay(int dateDay) {
+        this.dateDay = dateDay;
+    }
+
+    public int getDateMonth() {
+        return dateMonth;
+    }
+
+    public void setDateMonth(int dateMonth) {
+        this.dateMonth = dateMonth;
+    }
+
+    public int getDateYear() {
+        return dateYear;
+    }
+
+    public void setDateYear(int dateYear) {
+        this.dateYear = dateYear;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getFax() {
+        return fax;
+    }
+
+    public void setFax(int fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getMed() {
+        return med;
+    }
+
+    public void setMed(int med) {
+        this.med = med;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getBa() {
+        return ba;
+    }
+
+    public void setBa(int ba) {
+        this.ba = ba;
+    }
+
+    public int getLn() {
+        return ln;
+    }
+
+    public void setLn(int ln) {
+        this.ln = ln;
+    }
+
+    public int getVid() {
+        return vid;
+    }
+
+    public void setVid(int vid) {
+        this.vid = vid;
+    }
+
+    public String getLicence() {
+        return licence;
+    }
+
+    public void setLicence(String licence) {
+        this.licence = licence;
+    }
+
+    public int getDid() {
+        return did;
+    }
+
+    public void setDid(int did) {
+        this.did = did;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public int getIp1() {
+        return ip1;
+    }
+
+    public void setIp1(int ip1) {
+        this.ip1 = ip1;
+    }
+
+    public int getIp2() {
+        return ip2;
+    }
+
+    public void setIp2(int ip2) {
+        this.ip2 = ip2;
+    }
+
+    public int getIp3() {
+        return ip3;
+    }
+
+    public void setIp3(int ip3) {
+        this.ip3 = ip3;
+    }
+
+    public int getIp4() {
+        return ip4;
+    }
+
+    public void setIp4(int ip4) {
+        this.ip4 = ip4;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    
+    
+
     
     
     
