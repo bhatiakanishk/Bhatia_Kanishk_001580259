21caf7bb8a5a92ff12dc7b41b0b9923e25983f6f
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-09-26 18:46:41 -0400

    new

---
+++ Assignment Profile/src/model/Product.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.*;
+
+/**
+ *
+ * @author kanis
+ */
+public class Product {
+    Scanner sc=new Scanner(System.in);
+    
+    private String name; /*Name*/
+    private String location; /*Geographic data*/
+    private int dateDay; /*Day of Birth*/
+    private int dateMonth; /*Month of Birth*/
+    private int dateYear; /*Year of Birth*/
+    private int phone; /*Telephone number*/
+    private int fax; /*FAX number*/
+    private String email; /*Email address*/
+    private int ssn; /*Spcial Security Number*/ 
+    private int med; /*Medical Record Number*/
+    private int hp; /*Health plan number*/
+    private int ba; /*Bank Acoount Number*/
+    private int ln; /*Licence Number*/
+    private int vid; /*Vehicle identifier*/
+    private String licence;
+    private int did; /*Device identifier*/
+    private String link; /*LinkedIn*/
+    private int ip1; /*IP Address*/
+    private int ip2; /*IP Address*/
+    private int ip3; /*IP Address*/
+    private int ip4; /*IP Address*/
+    private int uid; /*Unique ID*/
+
+    
+    
+    
+}
