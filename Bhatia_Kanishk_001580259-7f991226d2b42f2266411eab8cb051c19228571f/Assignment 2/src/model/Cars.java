d925ab319be4d0d473e11cb424a5d81fb8a3affb
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-10-09 19:35:00 -0400

    Update Cars.java

--- Bhatia_Kanishk_001580259-7f991226d2b42f2266411eab8cb051c19228571f/Assignment 2/src/model/Cars.java
+++ Bhatia_Kanishk_001580259-7f991226d2b42f2266411eab8cb051c19228571f/Assignment 2/src/model/Cars.java
@@ -5,11 +5,15 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author kanishk
  */
 public class Cars {
+    
+    public ArrayList<Cars> cars;
     
     public String manufacturer; //Car Company
     public String model; //Car Model
@@ -19,24 +23,81 @@ public class Cars {
     public String city; //City 
     public String certificate; //Valid Maintenance Certificate
     public String available; //Availability
+  
 
-    //ma -> manufacturer
-    //md -> model
-    //yr -> year
-    //se -> seats
-    //sn -> Serial Number
-    //ci -> City
-    //cf -> Certificate
-    //av -> Availability
-    public Cars(String ma, String md, int yr, int se, String sn, String ci, String cf, String av) {
-        
-        this.manufacturer = ma;
-        this.model = md;
-        this.year = yr;
-        this.seats = se;
-        this.srno = sn;
-        this.city = ci;
-        this.certificate = cf;
-        this.available = av;
+    public Cars(String manufacturer, String model, int year, int seats, String srno, String city, String certificate, String available) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+        this.seats = seats;
+        this.srno = srno;
+        this.city = city;
+        this.certificate = certificate;
+        this.available = available;
+    }
+    
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public String getSrno() {
+        return srno;
+    }
+
+    public void setSrno(String srno) {
+        this.srno = srno;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    public String getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(String available) {
+        this.available = available;
     }
 }
