8e171503d292f2285662ce49973687943f6a90c2
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-10-30 17:21:38 -0400

    Create Patient_Directory.java

---
+++ Assignment 4/Assignment4/src/model/Patient_Directory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author kanis
+ */
+public class Patient_Directory extends Person_Directory {
+    List <Patient> patient = new ArrayList<>();
+
+	 public void addPatient(Patient p) {
+	        this.patient.add(p);
+	    }
+	 
+	 public void addPerson(Patient p) {
+		 this.person.add(p);
+	 }
+}
