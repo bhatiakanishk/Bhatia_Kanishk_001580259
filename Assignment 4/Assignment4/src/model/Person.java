f38de20fde2c8df9cc5cef265346abf44df1d3c0
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-10-30 17:17:14 -0400

    Create Person.java

---
+++ Assignment 4/Assignment4/src/model/Person.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.List;
+
+/**
+ *
+ * @author kanis
+ */
+public class Person extends Person_Directory {
+    public String firstName;
+    public String lastName;
+    public String contact;
+    public String zipcode;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public List<Person> getPerson() {
+        return person;
+    }
+
+    public void setPerson(List<Person> person) {
+        this.person = person;
+    }
+}
