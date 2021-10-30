3c792b970b790124608eb88fe89676a06641df77
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-10-30 17:21:45 -0400

    Create Community.java

---
+++ Assignment 4/Assignment4/src/model/Community.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author kanis
+ */
+public class Community {
+    List <House> houses = new ArrayList <House>();
+}
