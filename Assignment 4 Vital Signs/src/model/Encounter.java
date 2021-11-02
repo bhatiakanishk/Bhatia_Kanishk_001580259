3a408f2cbc8193002e842cbe112ac31b2bd02c8b
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-02 13:07:30 -0400

    Update Encounter.java

--- Assignment 4 Vital Signs/src/model/Encounter.java
+++ Assignment 4 Vital Signs/src/model/Encounter.java
@@ -12,14 +12,16 @@ import model.VitalSigns;
 public class Encounter extends VitalSigns{
     private String encounterDoc;
     private String encounterIssue;
+    private String encounterDate;
     
     public Encounter(){
     }
 
-    public Encounter(String encounterDoc, String encounterIssue, double temperature, double bloodpressure, int pulse, String date) {
-        super(temperature, bloodpressure, pulse, date);
+    public Encounter(String encounterDoc, String encounterIssue, String encounterDate, int temperature, int bloodpressure, int pulse, int weight) {
+        super(temperature, bloodpressure, pulse, weight);
         this.encounterDoc = encounterDoc;
         this.encounterIssue = encounterIssue;
+        this.encounterDate = encounterDate;
     }
 
     public String getEncounterDoc() {
@@ -37,7 +39,17 @@ public class Encounter extends VitalSigns{
     public void setEncounterIssue(String encounterIssue) {
         this.encounterIssue = encounterIssue;
     }
+
+    public String getEncounterDate() {
+        return encounterDate;
+    }
+
+    public void setEncounterDate(String encounterDate) {
+        this.encounterDate = encounterDate;
+    }
     
-    
-    
+    @Override
+    public String toString(){
+        return encounterDate;
+    }
 }
