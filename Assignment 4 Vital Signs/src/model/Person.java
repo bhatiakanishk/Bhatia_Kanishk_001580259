0dba2e45f9d4ac5e91dd95a6da6dda2194d70b86
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-02 13:07:28 -0400

    Update Person.java

--- Assignment 4 Vital Signs/src/model/Person.java
+++ Assignment 4 Vital Signs/src/model/Person.java
@@ -10,5 +10,94 @@ package model;
  * @author kanis
  */
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int contact;
+    private String isPatient;
+    private int house;
+    private String community;
+    private String city;
+    
+    public Person(){
+        
+    }
+    
+    public Person(String firstName, String lastName, int age, int contact, String isPatient, int house, String community, String city){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.contact = contact;
+        this.isPatient = isPatient;
+        this.house = house;
+        this.community = community;
+        this.city = city;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public String getIsPatient() {
+        return isPatient;
+    }
+
+    public void setIsPatient(String isPatient) {
+        this.isPatient = isPatient;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+
+    public String getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(String community) {
+        this.community = community;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    
     
 }
