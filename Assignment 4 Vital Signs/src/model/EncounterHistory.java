af5c07aa0cca0cbf565b67768d2d8d53b377f3d0
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-02 21:12:37 -0400

    Update EncounterHistory.java

--- Assignment 4 Vital Signs/src/model/EncounterHistory.java
+++ Assignment 4 Vital Signs/src/model/EncounterHistory.java
@@ -17,6 +17,13 @@ public class EncounterHistory {
         this.encounterArrayList = new ArrayList<Encounter>();
         Encounter en1 = new Encounter("Dr.A", "Headache", "20/10/2021", "100","100","95",85, "8879962666");
         Encounter en2 = new Encounter("Dr.B", "Fever", "25/10/2021", "90","90","90",89, "9969293888");
+        Encounter en3 = new Encounter("Dr.B", "Fever", "25/10/2021", "90","90","90",89, "9969293888");
+        Encounter en4 = new Encounter("Dr.B", "Fever", "25/10/2021", "90","90","90",89, "9969293888");
+        Encounter en5 = new Encounter("Dr.B", "Fever", "25/10/2021", "90","90","90",89, "9969293888");
+        Encounter en6 = new Encounter("Dr.B", "Fever", "25/10/2021", "90","90","90",89, "9969293888");
+        Encounter en7 = new Encounter("Dr.B", "Fever", "25/10/2021", "90","90","90",89, "9969293888");
+        Encounter en8 = new Encounter("Dr.B", "Fever", "25/10/2021", "90","90","90",89, "9969293888");
+        Encounter en9 = new Encounter("Dr.B", "Fever", "25/10/2021", "90","90","90",89, "9969293888");
         
         encounterArrayList.add(en1);
         encounterArrayList.add(en2);
