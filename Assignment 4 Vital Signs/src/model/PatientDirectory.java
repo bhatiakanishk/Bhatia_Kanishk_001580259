e7f381fd4d7476766d69eb551ab3209242f14d6b
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-02 22:25:24 -0400

    Update PatientDirectory.java

--- Assignment 4 Vital Signs/src/model/PatientDirectory.java
+++ Assignment 4 Vital Signs/src/model/PatientDirectory.java
@@ -25,15 +25,15 @@ public class PatientDirectory {
         this.patientArrayList = new ArrayList<Patient>();
         this.personData = new PersonDirectory();
         
-        Patient p1 = new Patient("A", "B", 22, "8879962666", "1", "Longwood", "Boston");
-        Patient p2 = new Patient("C", "D", 23, "9969293888", "1", "Longwood", "Boston");
-        Patient p3 = new Patient("E", "F", 23, "9969293888", "2", "Longwood", "Boston");
-        Patient p4 = new Patient("G", "H", 23, "9969293888", "2", "Longwood", "Boston");
-        Patient p5 = new Patient("I", "J", 23, "9969293888", "2", "Longwood", "Boston");
-        Patient p6 = new Patient("K", "L", 23, "9969293888", "3", "Longwood", "Boston");
-        Patient p7 = new Patient("M", "N", 23, "9969293888", "3", "Longwood", "Boston");
-        Patient p8 = new Patient("O", "P", 23, "9969293888", "4", "Longwood", "Boston");
-        Patient p9 = new Patient("Q", "R", 23, "9969293888", "4", "Longwood", "Boston");
+        Patient p1 = new Patient("A", "B", 25, "9825392454", "1", "Longwood", "Boston"); //Abnormal Age 25
+        Patient p2 = new Patient("C", "D", 29, "4676306147", "1", "Longwood", "Boston");//Abnormal Age 29
+        Patient p3 = new Patient("E", "F", 38, "5907806836", "2", "Longwood", "Boston");//Normal Age 38
+        Patient p4 = new Patient("G", "H", 40, "1189396615", "2", "Longwood", "Boston");//Normal Age 40
+        Patient p5 = new Patient("I", "J", 45, "8731701466", "2", "Longwood", "Boston");//Normal Age 45
+        Patient p6 = new Patient("K", "L", 52, "1924716878", "3", "Allston", "Boston");//Normal Age 52
+        Patient p7 = new Patient("M", "N", 59, "2504903702", "3", "Allston", "Boston");//Abnormal Age 59
+        Patient p8 = new Patient("O", "P", 68, "8772382293", "4", "Allston", "Boston");//Abnormal Age 68
+        Patient p9 = new Patient("Q", "R", 78, "5725987481", "4", "Allston", "Boston");//Abnormal Age 78
         
         patientArrayList.add(p1);
         patientArrayList.add(p2);
