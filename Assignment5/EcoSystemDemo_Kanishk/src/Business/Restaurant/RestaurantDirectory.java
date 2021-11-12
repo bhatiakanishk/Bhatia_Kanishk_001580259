25b380cd94a2bcc38e8de1cea5d93162f8c54af2
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-12 13:41:41 -0500

    Update RestaurantDirectory.java

--- Assignment5/EcoSystemDemo_Kanishk/src/Business/Restaurant/RestaurantDirectory.java
+++ Assignment5/EcoSystemDemo_Kanishk/src/Business/Restaurant/RestaurantDirectory.java
@@ -4,11 +4,59 @@
  * and open the template in the editor.
  */
 package Business.Restaurant;
-
+import Business.Restaurant.FinalOrder;
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
 /**
  *
- * @author harold
+ * @author kanishk
  */
 public class RestaurantDirectory {
+    ArrayList<Restaurant> rstrntList = new ArrayList<Restaurant>();
+
+    public ArrayList<Restaurant> getRstrntList() {
+        return rstrntList;
+    }
+
+    public void setRstrntList(ArrayList<Restaurant> rstrntList) {
+        this.rstrntList = rstrntList;
+    }
+    
+    public Restaurant getRestaurant(UserAccount ua){
+        for(Restaurant r: rstrntList){
+            if(ua.getUsername().equals(r.getUsrAcc().getUsername()))
+                return r;
+        }
+        return null;
+    }
+    
+    public void addRestaurant(Restaurant restaurant){
+        rstrntList.add(restaurant);
+    }
+    public void removeRestaurant(Restaurant restaurant){
+        rstrntList.remove(restaurant);
+    }
+    
+    int orderID = 0;
+    public int generateOrderId()
+    {
+        orderID++;
+        
+        return orderID;
+    }
+    
+    public FinalOrder getFinalOrder(String id)
+    {
+        for(Restaurant r: rstrntList)
+        {
+            ArrayList<FinalOrder> ordersAccepted = r.getOrdrAcptd();
+                for(FinalOrder fo: ordersAccepted)
+                {
+                    if(id.equals(fo.getOrderId()))
+                        return fo;
+                }
+        }
+        return null;
+    }
     
 }
