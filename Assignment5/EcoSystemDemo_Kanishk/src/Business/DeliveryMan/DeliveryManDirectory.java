ed867939134a514bb43a4f905fe1108e0316d571
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-12 12:48:06 -0500

    Update DeliveryManDirectory.java

--- Assignment5/EcoSystemDemo_Kanishk/src/Business/DeliveryMan/DeliveryManDirectory.java
+++ Assignment5/EcoSystemDemo_Kanishk/src/Business/DeliveryMan/DeliveryManDirectory.java
@@ -4,11 +4,38 @@
  * and open the template in the editor.
  */
 package Business.DeliveryMan;
-
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
 /**
  *
- * @author harold
+ * @author kanishk
  */
 public class DeliveryManDirectory {
+    ArrayList<DeliveryMan> deliveryDir = new ArrayList<DeliveryMan>();
     
+    public ArrayList<DeliveryMan> getDeliveryDir() {
+        return deliveryDir;
+    }
+
+    public void setDeliveryDir(ArrayList<DeliveryMan> deliveryDir) {
+        this.deliveryDir = deliveryDir;
+    }
+    
+    public void addDeliveryDir(DeliveryMan dm){
+        deliveryDir.add(dm);
+    }
+    
+    public void removeDeliveryDir(DeliveryMan dm){
+        deliveryDir.remove(dm);
+    }
+    
+    public DeliveryMan getDeliveryMan(UserAccount useracc)
+    {
+        for(DeliveryMan dm: deliveryDir)
+        {
+            if(dm.getName().equals(useracc.getUsername()))
+                return dm;
+        }
+        return null;
+    }
 }
