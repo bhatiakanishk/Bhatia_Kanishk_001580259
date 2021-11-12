8033336d3f8b6998d7833a7d898f5edf15d97907
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-12 12:32:23 -0500

    Update CustomerDirectory.java

--- Assignment5/EcoSystemDemo_Kanishk/src/Business/Customer/CustomerDirectory.java
+++ Assignment5/EcoSystemDemo_Kanishk/src/Business/Customer/CustomerDirectory.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package Business.Customer;
-
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
 /**
  *
- * @author harold
+ * @author kanishk
  */
 public class CustomerDirectory {
+    ArrayList<Customer> CstmrList = new ArrayList<Customer>();
+
+    public ArrayList<Customer> getCstmrList() {
+        return CstmrList;
+    }
+
+    public void setCstmrList(ArrayList<Customer> CstmrList) {
+        this.CstmrList = CstmrList;
+    }
     
+    public Customer getCustomer(UserAccount ua){
+        for(Customer c: CstmrList){
+            if(ua.getUsername().equals(c.getUsrAcc().getUsername()))
+                return c;
+        }
+        return null;
+    }
+    
+    public void addCustomer(Customer c){
+        this.CstmrList.add(c);
+    }
+    
+    public void deleteCustomer(Customer c){
+        this.CstmrList.remove(c);
+    }
 }
