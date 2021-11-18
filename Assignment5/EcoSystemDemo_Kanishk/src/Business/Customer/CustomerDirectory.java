e4c3eb38ea63b97ce4370d33f6200ef7e9f39804
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-18 14:13:12 -0500

    Update CustomerDirectory.java

--- Assignment5/EcoSystemDemo_Kanishk/src/Business/Customer/CustomerDirectory.java
+++ Assignment5/EcoSystemDemo_Kanishk/src/Business/Customer/CustomerDirectory.java
@@ -4,36 +4,66 @@
  * and open the template in the editor.
  */
 package Business.Customer;
-import Business.UserAccount.UserAccount;
+import Business.EcoSystem;
 import java.util.ArrayList;
 /**
  *
  * @author kanishk
  */
 public class CustomerDirectory {
-    ArrayList<Customer> CstmrList = new ArrayList<Customer>();
-
-    public ArrayList<Customer> getCstmrList() {
-        return CstmrList;
+    private ArrayList<Customer> customerDirectory;
+    
+    public CustomerDirectory(){
+        customerDirectory = new ArrayList<Customer>();
     }
 
-    public void setCstmrList(ArrayList<Customer> CstmrList) {
-        this.CstmrList = CstmrList;
+    public ArrayList<Customer> getCustomerDirectory() {
+        return customerDirectory;
+    }
+
+    public void setCustomerDirectory(ArrayList<Customer> customerDirectory) {
+        this.customerDirectory = customerDirectory;
     }
     
-    public Customer getCustomer(UserAccount ua){
-        for(Customer c: CstmrList){
-            if(ua.getUsername().equals(c.getUsrAcc().getUsername()))
-                return c;
+    public void deleteCustomer(int index,EcoSystem system){
+        customerDirectory.remove(index);
+    }
+    
+    public Customer add(String name, String phone, String address){
+        Customer customer1 = new Customer("C"+(customerDirectory.size()+1),name,phone,address);
+        customerDirectory.add(customer1);
+        return customer1;
+    }
+    
+    public Customer getCustomerId(int index){
+        return customerDirectory.get(index);
+    }
+    
+    public void updateCustomer(String id,String name,String phone,String address){
+        for(Customer customer: customerDirectory){
+            if(customer.getCustomerId().equalsIgnoreCase(id)){
+                customer.setName(name);
+                customer.setPhoneNo(phone);
+                customer.setAddress(address);
+            }
+        }
+    }
+    
+    public boolean isPhoneUnique(String phone){
+        for(Customer customer: customerDirectory){
+            if(customer.getPhoneNo().equalsIgnoreCase(phone)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public Customer getCustomer(String id){
+        for(Customer customer: customerDirectory){
+            if(customer.getCustomerId().equalsIgnoreCase(id)){
+                return customer;
+            }
         }
         return null;
-    }
-    
-    public void addCustomer(Customer c){
-        this.CstmrList.add(c);
-    }
-    
-    public void deleteCustomer(Customer c){
-        this.CstmrList.remove(c);
     }
 }
