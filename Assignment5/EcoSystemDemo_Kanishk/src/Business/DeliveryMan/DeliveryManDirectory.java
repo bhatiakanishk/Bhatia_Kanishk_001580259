daf28c010c7131c3b140bcb69e98c880c3f2d286
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-18 14:13:16 -0500

    Update DeliveryManDirectory.java

--- Assignment5/EcoSystemDemo_Kanishk/src/Business/DeliveryMan/DeliveryManDirectory.java
+++ Assignment5/EcoSystemDemo_Kanishk/src/Business/DeliveryMan/DeliveryManDirectory.java
@@ -4,37 +4,65 @@
  * and open the template in the editor.
  */
 package Business.DeliveryMan;
-import Business.UserAccount.UserAccount;
+import Business.EcoSystem;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 /**
  *
  * @author kanishk
  */
 public class DeliveryManDirectory {
-    ArrayList<DeliveryMan> deliveryDir = new ArrayList<DeliveryMan>();
+    private ArrayList<DeliveryMan> deliveryManDirectory;
+
+    public DeliveryManDirectory(){
+        deliveryManDirectory = new ArrayList<DeliveryMan>();
+    }
     
-    public ArrayList<DeliveryMan> getDeliveryDir() {
-        return deliveryDir;
+    public ArrayList<DeliveryMan> getDeliveryManDirectory() {
+        return deliveryManDirectory;
     }
 
-    public void setDeliveryDir(ArrayList<DeliveryMan> deliveryDir) {
-        this.deliveryDir = deliveryDir;
+    public void setDeliveryManDirectory(ArrayList<DeliveryMan> deliveryManDirectory) {
+        this.deliveryManDirectory = deliveryManDirectory;
     }
     
-    public void addDeliveryDir(DeliveryMan dm){
-        deliveryDir.add(dm);
+    public DeliveryMan add(String name, String phone){
+        DeliveryMan deliveryMan = new DeliveryMan("D"+(deliveryManDirectory.size()+1),name,phone);
+        deliveryManDirectory.add(deliveryMan);
+        return deliveryMan;
     }
     
-    public void removeDeliveryDir(DeliveryMan dm){
-        deliveryDir.remove(dm);
+    public void deleteDeliveryMan(int index,EcoSystem system){
+        deliveryManDirectory.remove(index);
     }
     
-    public DeliveryMan getDeliveryMan(UserAccount useracc)
-    {
-        for(DeliveryMan dm: deliveryDir)
-        {
-            if(dm.getName().equals(useracc.getUsername()))
-                return dm;
+    public DeliveryMan getDeliveryManId(int index){
+        return deliveryManDirectory.get(index);
+    }
+    
+    public boolean isPhoneUnique(String phone){
+        for(DeliveryMan deliveryMan: deliveryManDirectory){
+            if(deliveryMan.getPhoneNo().equalsIgnoreCase(phone)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public void updateDeliveryMan(String id,String name,String phone){
+        for(DeliveryMan deliveryMan: deliveryManDirectory){
+            if(deliveryMan.getDeliveryId().equalsIgnoreCase(id)){
+                deliveryMan.setName(name);
+                deliveryMan.setPhoneNo(phone);
+            }
+        }
+    }
+    
+    public DeliveryMan getDeliveryMan(String id){
+        for(DeliveryMan deliveryMan: deliveryManDirectory){
+            if(deliveryMan.getDeliveryId().equalsIgnoreCase(id)){
+                return deliveryMan;
+            }
         }
         return null;
     }
