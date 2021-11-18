a5f9a446e3b643d9c0f29334d3bc90a097a94305
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-18 14:37:39 -0500

    Update RestaurantDirectory.java

--- Assignment5/EcoSystemDemo_Kanishk/src/Business/Restaurant/RestaurantDirectory.java
+++ Assignment5/EcoSystemDemo_Kanishk/src/Business/Restaurant/RestaurantDirectory.java
@@ -4,59 +4,66 @@
  * and open the template in the editor.
  */
 package Business.Restaurant;
-import Business.Restaurant.FinalOrder;
-import Business.UserAccount.UserAccount;
+import Business.EcoSystem;
+import java.io.IOException;
 import java.util.ArrayList;
 /**
  *
  * @author kanishk
  */
 public class RestaurantDirectory {
-    ArrayList<Restaurant> rstrntList = new ArrayList<Restaurant>();
+    private ArrayList<Restaurant> restaurantDirectory;
 
-    public ArrayList<Restaurant> getRstrntList() {
-        return rstrntList;
-    }
-
-    public void setRstrntList(ArrayList<Restaurant> rstrntList) {
-        this.rstrntList = rstrntList;
+    public RestaurantDirectory() throws IOException{
+        restaurantDirectory = new ArrayList<Restaurant>();
     }
     
-    public Restaurant getRestaurant(UserAccount ua){
-        for(Restaurant r: rstrntList){
-            if(ua.getUsername().equals(r.getUsrAcc().getUsername()))
-                return r;
+    public ArrayList<Restaurant> getRestaurantDirectory() {
+        return restaurantDirectory;
+    }
+
+    public void setRestaurantDirectory(ArrayList<Restaurant> restaurantDirectory) {
+        this.restaurantDirectory = restaurantDirectory;
+    }
+    
+    public Restaurant add(String managerName,String name, String phone, String address){
+       Restaurant restaurant = new Restaurant("R"+(restaurantDirectory.size()+1),managerName, name, phone, address);
+       restaurantDirectory.add(restaurant);
+       return restaurant;
+    }
+    
+    public void deleteRestaurant(int index,EcoSystem system){
+        restaurantDirectory.remove(index);
+    }
+    
+    public Restaurant getRestaurantId(int index){
+        return restaurantDirectory.get(index);
+    }
+
+    public String getRestName(String restaurantId) {
+        for(Restaurant restaurant: restaurantDirectory){
+            if(restaurant.getRestaurantId().equalsIgnoreCase(restaurantId)){
+                return restaurant.getName();
+            }
+        }
+        return "";
+    }
+    
+    public Restaurant getRestaurant(String id){
+        for(Restaurant restaurant: restaurantDirectory){
+            if(restaurant.getRestaurantId().equalsIgnoreCase(id)){
+                return restaurant;
+            }
         }
         return null;
     }
     
-    public void addRestaurant(Restaurant restaurant){
-        rstrntList.add(restaurant);
-    }
-    public void removeRestaurant(Restaurant restaurant){
-        rstrntList.remove(restaurant);
-    }
-    
-    int orderID = 0;
-    public int generateOrderId()
-    {
-        orderID++;
-        
-        return orderID;
-    }
-    
-    public FinalOrder getFinalOrder(String id)
-    {
-        for(Restaurant r: rstrntList)
-        {
-            ArrayList<FinalOrder> ordersAccepted = r.getOrdrAcptd();
-                for(FinalOrder fo: ordersAccepted)
-                {
-                    if(id.equals(fo.getOrderId()))
-                        return fo;
-                }
+    public boolean isPhoneUnique(String phone){
+        for(Restaurant restaurant: restaurantDirectory){
+            if(restaurant.getPhoneNo().equalsIgnoreCase(phone)){
+                return false;
+            }
         }
-        return null;
+        return true;
     }
-    
 }
