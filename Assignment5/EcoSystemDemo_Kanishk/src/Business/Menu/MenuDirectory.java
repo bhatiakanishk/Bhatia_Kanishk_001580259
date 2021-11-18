e4c8c5445ac0089d57e941e9581496957295b9f4
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-18 14:28:56 -0500

    Create MenuDirectory.java

---
+++ Assignment5/EcoSystemDemo_Kanishk/src/Business/Menu/MenuDirectory.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Menu;
+import java.util.ArrayList;
+/**
+ *
+ * @author kanishk
+ */
+public class MenuDirectory {
+    private ArrayList<FoodItem> menuDirectory;
+    
+    public MenuDirectory(){
+        menuDirectory = new ArrayList<FoodItem>();
+    }
+
+    public ArrayList<FoodItem> getMenuDirectory() {
+        return menuDirectory;
+    }
+
+    public void setMenuDirectory(ArrayList<FoodItem> menuDirectory) {
+        this.menuDirectory = menuDirectory;
+    }
+
+    public void add(String restaurantId, String name, double price) {
+        FoodItem item = new FoodItem("F"+(menuDirectory.size()+1),restaurantId,name,price);
+        menuDirectory.add(item);
+    }
+    
+    public FoodItem getFoodItem(String itemId){
+        for(FoodItem item: menuDirectory){
+            if(item.getItemId().equalsIgnoreCase(itemId)){
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void updateFoodItem(String itemId, String name, Double price) {
+        for(FoodItem item: menuDirectory){
+            if(item.getItemId().equalsIgnoreCase(itemId)){
+                item.setItemName(name);
+                item.setPrice(price);
+            }
+        }
+    }
+    
+    public void deleteItem(String id){
+        for(int i =0; i< menuDirectory.size();i++){
+            if(menuDirectory.get(i).getItemId().equalsIgnoreCase(id)){
+                menuDirectory.remove(i);
+            }
+        }
+    }
+    
+    public FoodItem getFoodItemByIndex(int index){
+        return menuDirectory.get(index);
+    }
+}
