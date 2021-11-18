6f71581e0f7c5e2caf31bb4892c18db75291c76e
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2021-11-18 14:28:54 -0500

    Create Menu.java

---
+++ Assignment5/EcoSystemDemo_Kanishk/src/Business/Menu/Menu.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Menu;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kanishk
+ */
+public class Menu {
+    private ArrayList<FoodItem> menu;
+    
+    public Menu(){
+        menu = new ArrayList<FoodItem>();
+    }
+
+    public ArrayList<FoodItem> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(ArrayList<FoodItem> menu) {
+        this.menu = menu;
+    }
+}
